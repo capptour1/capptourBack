2a13db22ce47718d913c3631840d4906590c09de
capptour1 <capptour1@gmail.com>
2025-06-25 22:45:43 -0500

    Update and fix multer photos.js

--- src/api/photos.js
+++ src/api/photos.js
@@ -10,21 +10,20 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 // Configuración de Multer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(process.cwd(), 'uploads'));
-    },
-    filename: (req, file, cb) => {
-        const uniqueName = Date.now() + '-' + file.originalname;
-        cb(null, uniqueName);
-    },
-});
-
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(process.cwd(), 'uploads'));
+//     },
+//     filename: (req, file, cb) => {
+//         const uniqueName = Date.now() + '-' + file.originalname;
+//         cb(null, uniqueName);
+//     },
+// });
+const storage = multer.memoryStorage();//usamos memoria para evitar guardar archivos en disco y los trabajamos como buffers
 const upload = multer({
     storage,
     fileFilter: (req, file, cb) => {
         const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-        console.log('Mimetype recibido:', file.mimetype);  // 👉 Ayuda a depurar
         if (allowedTypes.includes(file.mimetype)) {
             cb(null, true);
         } else {
