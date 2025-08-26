118aefe4affd2d0034894b26e8c1ddcc4c8194ad
ALO922 <jvegadelgado@gmail.com>
2025-08-26 15:33:13 -0500

    Fix: Add nodeEnv to conect db.

--- src/db.js
+++ src/db.js
@@ -2,20 +2,13 @@ import 'dotenv/config';
 import pkg from 'pg';
 const { Pool } = pkg;
 
-console.log('🔍 Variables de entorno:', {
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  NODE_ENV: process.env.NODE_ENV
-});
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  nodeEnv: process.env.NODE_ENV,
   ssl: { rejectUnauthorized: false }
 });
 
