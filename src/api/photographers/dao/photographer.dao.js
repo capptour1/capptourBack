855e8f7c95996ea9f20f6f0fc2d2d16989fbf00a
Camilo Rosero <edisonrosero2106@gmail.com>
2025-11-27 16:34:07 -0500

    feat: enhance get_photographer_by_id function to include additional fields and improve error handling

--- src/api/photographers/dao/photographer.dao.js
+++ src/api/photographers/dao/photographer.dao.js
@@ -7,15 +7,23 @@ const start_transaction = () => {
 
 const get_photographer_by_id = async (userId) => {
     try {
-        console.log('Register client controller called');
+        console.log('Get photographer by ID DAO called', userId);
 
         const result = await sequelize.query(
-            `SELECT u.id AS userId, u.nombre_completo, TRUE AS estado, fp.ubicacion, fp.thumbnail
+            `SELECT u.id AS userId, u.nombre_completo, u.email as correo ,TRUE AS estado, fp.ubicacion, 
+            fp.descripcion, fp.precio_hora_cop, fp.precio_hora_usd, fp.precio_foto_cop, fp.precio_foto_usd,
+            contacto.detalle as telefono, fp.thumbnail
             FROM auth.usuarios u 
             INNER JOIN fotografo.fotografos f ON
             u.id = f.usuario_id
             INNER JOIN fotografo.foto_portafolio fp ON
             f.id = fp.id_fotografo
+            LEFT JOIN (
+                SELECT *,
+                ROW_NUMBER() OVER (PARTITION BY c.id_usuario ORDER BY c.id_contacto DESC) AS rn
+                FROM auth.contacto c
+                WHERE c.id_usuario = cast(:userId AS int) and c.tipo = 2            
+            ) AS contacto ON contacto.id_usuario = u.id AND contacto.rn = 1
             WHERE u.rol_id = 5 AND u.id = cast(:userId AS int)
             `,
             {
@@ -24,13 +32,12 @@ const get_photographer_by_id = async (userId) => {
             }
         );
 
-        return result[0][0];
+        return result[0];
     } catch (error) {
-        console.error('Error registering client:', error);
-        throw new Error('Error al registrar el cliente');
+        console.error('Error getting photographer by ID:', error);
+        throw new Error('Error al obtener la información del fotógrafo');
     }
 };
-
 
 
 export default {
