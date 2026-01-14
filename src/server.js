8e1fb34d3d44612ccb7a6995188111b5ddedc805
gabri32 <gabodelac@gmail.com>
2026-01-13 22:25:05 -0500

    ping for server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 import http from 'http';
 import app from './app.js';
 import initSockets from './sockets/index.js';
-
 const server = http.createServer(app);
+import https from "https";
 
 // Inicializar sockets
 initSockets(server);
-
+setInterval(() => {
+  https.get("https://capptourback.onrender.com", (res) => {
+    console.log(`Ping enviado, status code: ${res.statusCode}`);
+  }).on("error", (err) => {
+    console.error("Error en auto-ping:", err.message);
+  });
+}, 7000); // 120,000 ms = 2 minutos
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
